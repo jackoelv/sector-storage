56661a548861e69aebe811aed014bb8f754eac89
jack <jackoelv@qq.com>
2020-07-14 09:31:11 +0800

    restore sched

--- sched.go
+++ sched.go
@@ -44,15 +44,11 @@ type WorkerSelector interface {
 	Cmp(ctx context.Context, task sealtasks.TaskType, a, b *workerHandle) (bool, error) // true if a is preferred over b
 }
 
-<<<<<<< Updated upstream
-type sectorsNum int
-=======
 //add by jackoelv
 type workerState struct {
 	sector   abi.SectorID
 	taskType sealtasks.TaskType
 }
->>>>>>> Stashed changes
 
 type scheduler struct {
 	spt abi.RegisteredSealProof
@@ -61,13 +57,8 @@ type scheduler struct {
 	nextWorker WorkerID
 	workers    map[WorkerID]*workerHandle
 
-<<<<<<< Updated upstream
-	workerSectorNum  map[WorkerID]sectorsNum   //add by jackoelv
-	workerBySectorID map[abi.SectorID]WorkerID //add by jackoelv
-=======
 	workerParallelSectors map[WorkerID]int           //add by jackoelv
 	workerDoing           map[WorkerID][]workerState //add by jackoelv
->>>>>>> Stashed changes
 
 	newWorkers chan *workerHandle
 
@@ -88,13 +79,8 @@ func newScheduler(spt abi.RegisteredSealProof) *scheduler {
 		nextWorker: 0,
 		workers:    map[WorkerID]*workerHandle{},
 
-<<<<<<< Updated upstream
-		workerSectorNum:  map[WorkerID]sectorsNum{},   //add by jackoelv
-		workerBySectorID: map[abi.SectorID]WorkerID{}, //add by jackoelv
-=======
 		workerParallelSectors: map[WorkerID]int{},
 		workerDoing:           map[WorkerID][]workerState{},
->>>>>>> Stashed changes
 
 		newWorkers: make(chan *workerHandle),
 
@@ -192,12 +178,10 @@ func (sh *scheduler) runSched() {
 	for {
 		select {
 		case w := <-sh.newWorkers:
-			// log.Warnf("jackoelv:sched:newWorkers")
 			sh.schedNewWorker(w)
 		case wid := <-sh.workerClosing:
 			sh.schedDropWorker(wid)
 		case req := <-sh.schedule:
-			// log.Warnf("jackoelv:sched:schedule then maybeSchedRequest")
 			scheduled, err := sh.maybeSchedRequest(req)
 			if err != nil {
 				req.respond(err)
@@ -256,7 +240,6 @@ func (sh *scheduler) onWorkerFreed(wid WorkerID) {
 var selectorTimeout = 5 * time.Second
 
 func (sh *scheduler) maybeSchedRequest(req *workerRequest) (bool, error) {
-	// log.Warnf("jackoelv:sched:maybeSchedRequest")
 	sh.workersLk.Lock()
 	defer sh.workersLk.Unlock()
 
@@ -265,28 +248,14 @@ func (sh *scheduler) maybeSchedRequest(req *workerRequest) (bool, error) {
 
 	needRes := ResourceTable[req.taskType][sh.spt]
 
-	//add by jackoelv begin
-
-	if req.taskType != sealtasks.TTAddPiece && req.taskType != sealtasks.TTFetch && req.taskType != sealtasks.TTUnseal && req.taskType != sealtasks.TTReadUnsealed {
-		wid := sh.workerBySectorID[req.sector]
-		if (wid > 0) && (sh.workerSectorNum[wid-1] <= 3) {
-			log.Warnf("jackoelv:maybeSchedRequest:!!!NOT!!!:sectorNumber: %s,taskType: %s, wid: %d, totalSectorsNum:%s", req.sector.Number, req.taskType, wid-1, sh.workerSectorNum[wid-1])
-			return true, sh.assignWorker(wid-1, sh.workers[wid-1], req)
-		}
-	}
-	if req.taskType == sealtasks.TTAddPiece {
-		log.Warnf("jackoelv:maybeSchedRequest:TTAddPiece:sectorNumber: %s,taskType: %s, wid: 0", req.sector.Number, req.taskType)
-		return true, sh.assignWorker(0, sh.workers[0], req)
-	}
-	//add by jack end
-
 	for wid, worker := range sh.workers {
+		// Test if precommit 1 to otherworker what will happen?
+		if wid != 0 {
+			return true, sh.assignWorker(wid, sh.workers[wid], req)
+		}
 		rpcCtx, cancel := context.WithTimeout(req.ctx, selectorTimeout)
-		// log.Warnf("jackoelv:sched:maybeSchedRequest:req.taskType: %s, wid: %s, ok: %s",req.taskType,wid,ok)
 		ok, err := req.sel.Ok(rpcCtx, req.taskType, sh.spt, worker)
-
-		log.Warnf("jackoelv:sched:maybeSchedRequest:Orginal:req.taskType: %s, wid: %s, ok: %s", req.taskType, wid, ok)
-=======
+		log.Warnf("jackoelvAddpiecetest:sched:WorkerID:%d,req.sel.Ok:%s,req.taskType:%s", wid, ok, req.taskType)
 		cancel()
 
 		if err != nil {
@@ -298,14 +267,16 @@ func (sh *scheduler) maybeSchedRequest(req *workerRequest) (bool, error) {
 		}
 		tried++
 
-		if !canHandleRequest(needRes, sh.spt, wid, worker.info.Resources, worker.preparing) {
-			log.Warnf("jackoelv:sched:maybeSchedRequest:Orginal:canHandleRequest,wid: %d", wid)
+		if !canHandleRequest(needRes, wid, worker.info.Resources, worker.preparing) {
 			continue
 		}
+
 		acceptable = append(acceptable, wid)
 	}
 
 	if len(acceptable) > 0 {
+		log.Warnf("jackoelvAddpiecetest:sched:len(acceptable):%d,req.taskType:%s", len(acceptable), req.taskType)
+
 		{
 			var serr error
 
@@ -313,7 +284,6 @@ func (sh *scheduler) maybeSchedRequest(req *workerRequest) (bool, error) {
 				rpcCtx, cancel := context.WithTimeout(req.ctx, selectorTimeout)
 				defer cancel()
 				r, err := req.sel.Cmp(rpcCtx, req.taskType, sh.workers[acceptable[i]], sh.workers[acceptable[j]])
-				// log.Warnf("jackoelv:sched:maybeSchedRequest:acceptable,winner r: %s",r)
 
 				if err != nil {
 					serr = multierror.Append(serr, err)
@@ -325,7 +295,7 @@ func (sh *scheduler) maybeSchedRequest(req *workerRequest) (bool, error) {
 				return false, xerrors.Errorf("error(s) selecting best worker: %w", serr)
 			}
 		}
-		log.Warnf("jackoelv:sched:maybeSchedRequest:Orginal:return assignWorker true,acceptable[0]: %d; sector: %d", acceptable[0], req.sector)
+
 		return true, sh.assignWorker(acceptable[0], sh.workers[acceptable[0]], req)
 	}
 
@@ -337,7 +307,6 @@ func (sh *scheduler) maybeSchedRequest(req *workerRequest) (bool, error) {
 }
 
 func (sh *scheduler) assignWorker(wid WorkerID, w *workerHandle, req *workerRequest) error {
-	log.Warnf("jackoelv:sched:assignWorker,WorkerID:%d ; taskType: %s; sector: %d", wid, req.taskType, req.sector)
 	needRes := ResourceTable[req.taskType][sh.spt]
 
 	w.preparing.add(w.info.Resources, needRes)
@@ -365,8 +334,8 @@ func (sh *scheduler) assignWorker(wid WorkerID, w *workerHandle, req *workerRequ
 			}
 			return
 		}
-		log.Warnf("jackoelv:sched:assignWorker:ask worker for withResources,ID: %d", wid)
-		err = w.active.withResources(sh.spt, wid, w.info.Resources, needRes, &sh.workersLk, func() error {
+
+		err = w.active.withResources(wid, w.info.Resources, needRes, &sh.workersLk, func() error {
 			w.preparing.free(w.info.Resources, needRes)
 			sh.workersLk.Unlock()
 			defer sh.workersLk.Lock() // we MUST return locked from this function
@@ -387,23 +356,6 @@ func (sh *scheduler) assignWorker(wid WorkerID, w *workerHandle, req *workerRequ
 			}
 
 			//when first pre1 coming,then assign the sector to the worker
-
-			//Add by jackoelv begin
-			log.Warnf("jackoelv:sched:assignWorker:modify state for the last worker")
-
-			switch req.taskType {
-			case sealtasks.TTPreCommit1:
-				log.Warnf("jackoelv:sched:assignWorker:initialize the last worker: wid: %d, sectorNumber: %s", wid, req.sector.Number)
-				tmpWorkerID := sh.workerBySectorID[req.sector]
-				tmpWorkerID = wid + 1
-				sh.workerBySectorID[req.sector] = tmpWorkerID
-				sh.workerSectorNum[wid]++
-			case sealtasks.TTCommit2:
-				sh.workerSectorNum[wid]--
-			}
-			log.Warnf("jackoelv:worker:%d in sector:%s,SectorNumTotal:%d", wid, req.sector.Number, sh.workerSectorNum[wid])
-
-			//add by jackoelv end
 
 			select {
 			case req.ret <- workerResponse{err: err}:
@@ -427,11 +379,6 @@ func (sh *scheduler) assignWorker(wid WorkerID, w *workerHandle, req *workerRequ
 	return nil
 }
 
-<<<<<<< Updated upstream
-func (a *activeResources) withResources(spt abi.RegisteredSealProof, id WorkerID, wr storiface.WorkerResources, r Resources, locker sync.Locker, cb func() error) error {
-	// log.Warnf("jackoelv:sched:withResources")
-	for !canHandleRequest(r, spt, id, wr, a) {
-=======
 func (sh *scheduler) changeWorkState(wid WorkerID, sector abi.SectorID, taskType sealtasks.TaskType) error {
 	found := false
 	wStates := sh.workerDoing[wid]
@@ -472,7 +419,6 @@ func (sh *scheduler) removeWorkState(wid WorkerID, sector abi.SectorID) error {
 
 func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResources, r Resources, locker sync.Locker, cb func() error) error {
 	for !canHandleRequest(r, id, wr, a) {
->>>>>>> Stashed changes
 		if a.cond == nil {
 			a.cond = sync.NewCond(locker)
 		}
@@ -517,35 +463,22 @@ func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {
 	a.memUsedMax -= r.MaxMemory
 }
 
-func canHandleRequest(needRes Resources, spt abi.RegisteredSealProof, wid WorkerID, res storiface.WorkerResources, active *activeResources) bool {
-	// log.Warnf("jackoelv:sched:canHandleRequest")
+func canHandleRequest(needRes Resources, wid WorkerID, res storiface.WorkerResources, active *activeResources) bool {
+
 	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)
 	minNeedMem := res.MemReserved + active.memUsedMin + needRes.MinMemory + needRes.BaseMinMemory
-	// log.Warnf("jackoelv:sched:canHandleRequest:minNeedMem:%d", minNeedMem)
-	// log.Warnf("jackoelv:sched:canHandleRequest:MemPhysical:%d", res.MemPhysical)
 	if minNeedMem > res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %d; not enough physical memory - need: %dM, have %dM", wid, minNeedMem/mib, res.MemPhysical/mib)
 		return false
 	}
 
 	maxNeedMem := res.MemReserved + active.memUsedMax + needRes.MaxMemory + needRes.BaseMinMemory
-	// log.Warnf("jackoelv:sched:canHandleRequest:maxNeedMem:%d", maxNeedMem)
-	if spt == abi.RegisteredSealProof_StackedDrg32GiBV1 {
-		maxNeedMem += MaxCachingOverhead
-	}
-	if spt == abi.RegisteredSealProof_StackedDrg64GiBV1 {
-		maxNeedMem += MaxCachingOverhead * 2 // ewwrhmwh
-	}
-	// log.Warnf("jackoelv:sched:canHandleRequest:MaxCachingOverhead:%d", MaxCachingOverhead)
-	// log.Warnf("jackoelv:sched:canHandleRequest:maxNeedMem:%d", maxNeedMem)
-	// log.Warnf("jackoelv:sched:canHandleRequest:res.MemSwap+res.MemPhysical:%d", res.MemSwap+res.MemPhysical)
+
 	if maxNeedMem > res.MemSwap+res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %d; not enough virtual memory - need: %dM, have %dM", wid, maxNeedMem/mib, (res.MemSwap+res.MemPhysical)/mib)
 		return false
 	}
-	// log.Warnf("jackoelv:sched:canHandleRequest: active.cpuUse :%d", active.cpuUse)
-	// log.Warnf("jackoelv:sched:canHandleRequest: uint64(needRes.Threads) :%d", uint64(needRes.Threads))
-	// log.Warnf("jackoelv:sched:canHandleRequest: res.CPUs :%d", res.CPUs)
+
 	if needRes.MultiThread() {
 		if active.cpuUse > 0 {
 			log.Debugf("sched: not scheduling on worker %d; multicore process needs %d threads, %d in use, target %d", wid, res.CPUs, active.cpuUse, res.CPUs)
@@ -557,8 +490,7 @@ func canHandleRequest(needRes Resources, spt abi.RegisteredSealProof, wid Worker
 			return false
 		}
 	}
-	// log.Warnf("jackoelv:sched:canHandleRequest: len(res.GPUs)  :%d", len(res.GPUs))
-	// log.Warnf("jackoelv:sched:canHandleRequest: active.gpuUsed  :%d", active.gpuUsed)
+
 	if len(res.GPUs) > 0 && needRes.CanGPU {
 		if active.gpuUsed {
 			log.Debugf("sched: not scheduling on worker %d; GPU in use", wid)
